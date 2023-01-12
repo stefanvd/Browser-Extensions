cd5ad0e46f1330ecbf78de8ce8f973fb0e286e1b
Stefan Van Damme <20127018+stefanvd@users.noreply.github.com>
2023-01-12 23:03:31 +0100

    Add Chrome

---
+++ Note-Sidebar/Note-Sidebar-for-Chrome/src/scripts/background.js
@@ -0,0 +1,280 @@
+//================================================
+/*
+
+Note Sidebar
+Simple note sidebar which can be used to write a note, record thoughts, to-do list, meeting notes, etc.
+Copyright (C) 2022 Stefan vd
+www.stefanvd.net
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+To view a copy of this license, visit http://creativecommons.org/licenses/GPL/2.0/
+
+*/
+//================================================
+
+if(typeof browser !== "undefined"){
+	var qtest = browser.sidebarAction;
+	if(typeof qtest !== "undefined"){
+		browser.browserAction.onClicked.addListener(function(){
+			browser.sidebarAction.toggle();
+		});
+	}
+}
+
+// Importing the constants
+// eslint-disable-next-line no-undef
+importScripts("constants.js");
+
+chrome.runtime.onMessage.addListener(function request(request){
+	// eye protection & autodim & shortcut
+	switch(request.name){
+	case"bckreload":
+		installation();
+		break;
+	}
+	return true;
+});
+
+var i18nfirsttext = chrome.i18n.getMessage("firsttext");
+
+// contextMenus
+function onClickHandler(info){
+	if(info.menuItemId == "totlguideemenu"){
+		chrome.tabs.create({url: linkguide, active:true});
+	}else if(info.menuItemId == "totldevelopmenu"){
+		chrome.tabs.create({url: linkdonate, active:true});
+	}else if(info.menuItemId == "totlratemenu"){
+		chrome.tabs.create({url: writereview, active:true});
+	}else if(info.menuItemId == "totlshareemail"){
+		var sturnoffthelightemail = "mailto:your@email.com?subject=" + chrome.i18n.getMessage("sharetexta") + "&body=" + chrome.i18n.getMessage("sharetextb") + " " + linkproduct; chrome.tabs.create({url: sturnoffthelightemail, active:true});
+	}else if(info.menuItemId == "totlsharetwitter"){
+		var slinkproductcodeurl = encodeURIComponent(chrome.i18n.getMessage("sharetextd") + " " + linkproduct); chrome.tabs.create({url: "https://twitter.com/intent/tweet?text=" + slinkproductcodeurl, active:true});
+	}else if(info.menuItemId == "totlsharefacebook"){
+		chrome.tabs.create({url: "https://www.facebook.com/sharer/sharer.php?u=" + linkproduct, active:true});
+	}else if(info.menuItemId == "totlsubscribe"){
+		chrome.tabs.create({url: linkyoutube, active:true});
+	}else if(info.menuItemId == "totlshareqq"){
+		chrome.tabs.create({url: "https://connect.qq.com/widget/shareqq/index.html?url=" + encodeURIComponent(linkproduct) + "&title=" + encodeURIComponent(chrome.i18n.getMessage("sharetextd")), active:true});
+	}else if(info.menuItemId == "totlshareweibo"){
+		chrome.tabs.create({url: "https://service.weibo.com/share/share.php?url=" + linkproduct + "&title=" + encodeURIComponent(chrome.i18n.getMessage("sharetextd")), active:true});
+	}else if(info.menuItemId == "totlsharevkontakte"){
+		chrome.tabs.create({url: "https://vk.com/share.php?url=" + linkproduct, active:true});
+	}else if(info.menuItemId == "totlsharewhatsapp"){
+		chrome.tabs.create({url: "https://api.whatsapp.com/send?text=" + chrome.i18n.getMessage("sharetextd") + "%0a" + linkproduct, active:true});
+	}else if(info.menuItemId == "snpage"){
+		var selectedtext = info.selectionText;
+		chrome.storage.sync.get(["txtvalue"], function(items){
+			var theValue = items["txtvalue"]; if(theValue == null){ theValue = i18nfirsttext; }
+			var newtextstring = theValue + " " + selectedtext;
+			chrome.storage.sync.set({"txtvalue": newtextstring});
+		});
+	}
+}
+
+// check to remove all contextmenus
+if(chrome.contextMenus){
+	chrome.contextMenus.removeAll(function(){
+	// console.log("contextMenus.removeAll callback");
+	});
+}
+
+var sharemenusharetitle = chrome.i18n.getMessage("sharemenusharetitle");
+var sharemenuwelcomeguidetitle = chrome.i18n.getMessage("sharemenuwelcomeguidetitle");
+var sharemenutellafriend = chrome.i18n.getMessage("sharemenutellafriend");
+var sharemenusendatweet = chrome.i18n.getMessage("sharemenusendatweet");
+var sharemenupostonfacebook = chrome.i18n.getMessage("sharemenupostonfacebook");
+var sharemenuratetitle = chrome.i18n.getMessage("sharemenuratetitle");
+var sharemenudonatetitle = chrome.i18n.getMessage("sharemenudonatetitle");
+var sharemenusubscribetitle = chrome.i18n.getMessage("desremyoutube");
+var sharemenupostonweibo = chrome.i18n.getMessage("sharemenupostonweibo");
+var sharemenupostonvkontakte = chrome.i18n.getMessage("sharemenupostonvkontakte");
+var sharemenupostonwhatsapp = chrome.i18n.getMessage("sharemenupostonwhatsapp");
+var sharemenupostonqq = chrome.i18n.getMessage("sharemenupostonqq");
+
+function browsercontext(a, b, c, d){
+	var item = {"title": a, "type": "normal", "id": b, "contexts": contexts};
+	var newitem;
+	if(d != ""){
+		item = Object.assign({}, item, {parentId: d});
+	}
+	if(c != ""){
+		newitem = Object.assign({}, item, {icons: c});
+	}
+	try{
+		// try show web browsers that do support "icons"
+		// Firefox, Opera, Microsoft Edge
+		return chrome.contextMenus.create(newitem);
+	}catch(e){
+		// catch web browsers that do NOT show the icon
+		// Google Chrome
+		return chrome.contextMenus.create(item);
+	}
+}
+
+var actionmenuadded = false;
+if(chrome.contextMenus){
+	if(actionmenuadded == false){
+		actionmenuadded = true;
+
+		var contexts = ["browser_action"];
+
+		browsercontext(sharemenuwelcomeguidetitle, "totlguideemenu", {"16": "images/IconGuide.png", "32": "images/IconGuide@2x.png"});
+		browsercontext(sharemenudonatetitle, "totldevelopmenu", {"16": "images/IconDonate.png", "32": "images/IconDonate@2x.png"});
+		browsercontext(sharemenuratetitle, "totlratemenu", {"16": "images/IconStar.png", "32": "images/IconStar@2x.png"});
+
+		// Create a parent item and two children.
+		var parent = null;
+		parent = browsercontext(sharemenusharetitle, "totlsharemenu", {"16": "images/IconShare.png", "32": "images/IconShare@2x.png"});
+		browsercontext(sharemenutellafriend, "totlshareemail", {"16": "images/IconEmail.png", "32": "images/IconEmail@2x.png"}, parent);
+		chrome.contextMenus.create({"title": "", "type":"separator", "id": "totlsepartorshare", "contexts": contexts, "parentId": parent});
+
+		var uiLanguage = chrome.i18n.getUILanguage();
+		if(uiLanguage.includes("zh")){
+			// Chinese users
+			browsercontext(sharemenupostonweibo, "totlshareweibo", {"16": "images/IconWeibo.png", "32": "images/IconWeibo@2x.png"}, parent);
+			browsercontext(sharemenupostonqq, "totlshareqq", {"16": "images/IconQQ.png", "32": "images/IconQQ@2x.png"}, parent);
+		}else if(uiLanguage.includes("ru")){
+			// Russian users
+			browsercontext(sharemenupostonvkontakte, "totlsharevkontakte", {"16": "images/IconVkontakte.png", "32": "images/IconVkontakte@2x.png"}, parent);
+			browsercontext(sharemenupostonfacebook, "totlsharefacebook", {"16": "images/IconFacebook.png", "32": "images/IconFacebook@2x.png"}, parent);
+			browsercontext(sharemenupostonwhatsapp, "totlsharewhatsapp", {"16": "images/IconWhatsApp.png", "32": "images/IconWhatsApp@2x.png"}, parent);
+			browsercontext(sharemenusendatweet, "totlsharetwitter", {"16": "images/IconTwitter.png", "32": "images/IconTwitter@2x.png"}, parent);
+		}else{
+			// all users
+			browsercontext(sharemenupostonfacebook, "totlsharefacebook", {"16": "images/IconFacebook.png", "32": "images/IconFacebook@2x.png"}, parent);
+			browsercontext(sharemenupostonwhatsapp, "totlsharewhatsapp", {"16": "images/IconWhatsApp.png", "32": "images/IconWhatsApp@2x.png"}, parent);
+			browsercontext(sharemenusendatweet, "totlsharetwitter", {"16": "images/IconTwitter.png", "32": "images/IconTwitter@2x.png"}, parent);
+		}
+
+		chrome.contextMenus.create({"title": "", "type":"separator", "id": "totlsepartor", "contexts": contexts});
+		browsercontext(sharemenusubscribetitle, "totlsubscribe", {"16": "images/IconYouTube.png", "32": "images/IconYouTube@2x.png"});
+
+		chrome.contextMenus.onClicked.addListener(onClickHandler);
+	}
+}
+
+var contextmenus;
+chrome.storage.sync.get(["contextmenus"], function(items){
+	contextmenus = items.contextmenus; if(contextmenus == null)contextmenus = true;
+	if(items["contextmenus"]){ checkcontextmenus(); }
+});
+
+// context menu for page
+var menupage = null;
+var contextmenuadded = false;
+var contextarraypage = [];
+var contextspage = null;
+
+var pagetitle = chrome.i18n.getMessage("pagetitle");
+function checkcontextmenus(){
+	if(chrome.contextMenus){
+		if(contextmenuadded == false){
+			contextmenuadded = true;
+
+			// page
+			contextspage = ["selection"];
+			menupage = chrome.contextMenus.create({"title": pagetitle, "type":"normal", "id": "snpage", "contexts":contextspage});
+			contextarraypage.push(menupage);
+		}
+	}
+}
+
+function removecontexmenus(){
+	if(contextspage != null){
+		chrome.contextMenus.remove("snpage");
+	}
+	contextspage = null;
+	contextmenuadded = false;
+}
+
+function onchangestorage(a, b, c, d){
+	if(a[b]){
+		if(a[b].newValue == true){ c(); }else{ d(); }
+	}
+}
+
+chrome.storage.onChanged.addListener(function(changes){
+	onchangestorage(changes, "contextmenus", checkcontextmenus, removecontexmenus);
+	if(changes["txtvalue"]){
+		if(changes["txtvalue"].newValue){ chrome.runtime.sendMessage({msg: "setnotetext", value: changes["txtvalue"].newValue}); }
+	}
+	if(changes["counter"]){
+		chrome.runtime.sendMessage({msg: "setcounter", value: changes["counter"].newValue});
+	}
+	if(changes["copy"]){
+		chrome.runtime.sendMessage({msg: "setcopy", value: changes["copy"].newValue});
+	}
+	if(changes["speech"]){
+		chrome.runtime.sendMessage({msg: "setspeech", value: changes["speech"].newValue});
+	}
+	if(changes["voices"]){
+		chrome.runtime.sendMessage({msg: "setvoices", value: changes["voices"].newValue});
+	}
+	if(changes["fontsize"]){
+		chrome.runtime.sendMessage({msg: "setfontsize", value: changes["fontsize"].newValue});
+	}
+	if(changes["lineheight"]){
+		chrome.runtime.sendMessage({msg: "setlineheight", value: changes["lineheight"].newValue});
+	}
+	if(changes["colorlight"]){
+		chrome.runtime.sendMessage({msg: "setcolorlight", value: changes["colorlight"].newValue});
+	}
+	if(changes["colordark"]){
+		chrome.runtime.sendMessage({msg: "setcolordark", value: changes["colordark"].newValue});
+	}
+	if(changes["backgroundlight"]){
+		chrome.runtime.sendMessage({msg: "setbackgroundlight", value: changes["backgroundlight"].newValue});
+	}
+	if(changes["backgrounddark"]){
+		chrome.runtime.sendMessage({msg: "setbackgrounddark", value: changes["backgrounddark"].newValue});
+	}
+	if(changes["backgroundcolor"]){
+		chrome.runtime.sendMessage({msg: "setbackgroundcolor", value: changes["backgroundcolor"].newValue});
+	}
+	if(changes["backgroundimage"]){
+		chrome.runtime.sendMessage({msg: "setbackgroundimage", value: changes["backgroundimage"].newValue});
+	}
+	if(changes["backgroundsource"]){
+		chrome.runtime.sendMessage({msg: "setbackgroundsource", value: changes["backgroundsource"].newValue});
+	}
+	if(changes["backgroundsize"]){
+		chrome.runtime.sendMessage({msg: "setbackgroundsize", value: changes["backgroundsize"].newValue});
+	}
+	if(changes["print"]){
+		chrome.runtime.sendMessage({msg: "setprint", value: changes["print"].newValue});
+	}
+});
+
+chrome.runtime.setUninstallURL(linkuninstall);
+
+function initwelcome(){
+	chrome.storage.sync.get(["firstRun"], function(chromeset){
+		if((chromeset["firstRun"] != "false") && (chromeset["firstRun"] != false)){
+			chrome.tabs.create({url: linkwelcome});
+			var crrinstall = new Date().getTime();
+			chrome.storage.sync.set({"firstRun": false, "version": "1.0", "firstDate": crrinstall});
+		}
+	});
+}
+
+function installation(){
+	initwelcome();
+}
+
+chrome.runtime.onInstalled.addListener(function(){
+	installation();
+});
---
+++ Note-Sidebar/Note-Sidebar-for-Chrome/src/scripts/panel.js
@@ -0,0 +1,439 @@
+//================================================
+/*
+
+Note Sidebar
+Simple note sidebar which can be used to write a note, record thoughts, to-do list, meeting notes, etc.
+Copyright (C) 2022 Stefan vd
+www.stefanvd.net
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+To view a copy of this license, visit http://creativecommons.org/licenses/GPL/2.0/
+
+*/
+//================================================
+
+var maintext; var theValue; var counter; var copy; var speech; var voices; var fontsize; var lineheight; var colorlight; var colordark; var backgroundlight; var backgrounddark; var backgroundcolor; var backgroundimage; var backgroundsource; var backgroundsize; var printicon; var password; var enterpassword;
+
+function save(){
+	theValue = maintext.value;
+	chrome.storage.sync.set({"txtvalue": theValue});
+}
+
+var i18nfirsttext = chrome.i18n.getMessage("firsttext");
+var i18ndefault = chrome.i18n.getMessage("titeldefault");
+var i18npasswordplaceholder = chrome.i18n.getMessage("passwordplaceholder");
+var i18ntitelcopytext = chrome.i18n.getMessage("titlecopytextdone");
+var i18ndescopytext = chrome.i18n.getMessage("descopytextdone");
+
+function focuspassword(){
+	document.getElementById("inputpass").focus();
+}
+
+function init(){
+	maintext = document.querySelector("#maintext");
+	chrome.storage.sync.get(["firstDate", "optionskipremember", "txtvalue", "counter", "copy", "speech", "voices", "fontsize", "lineheight", "colorlight", "colordark", "backgroundlight", "backgrounddark", "backgroundcolor", "backgroundimage", "backgroundsource", "backgroundsize", "print", "password", "enterpassword"], function(items){
+		theValue = items["txtvalue"]; if(theValue == null){ theValue = i18nfirsttext; }
+		counter = items["counter"]; if(counter == null){ counter = true; }
+		copy = items["copy"]; if(copy == null){ copy = true; }
+		speech = items["speech"]; if(speech == null){ speech = true; }
+		voices = items["voices"]; if(voices == null){ voices = false; }
+		fontsize = items["fontsize"]; if(fontsize == null){ fontsize = 18; }
+		lineheight = items["lineheight"]; if(lineheight == null){ lineheight = 26; }
+		colorlight = items["colorlight"]; if(colorlight == null){ colorlight = "#373737"; }
+		colordark = items["colordark"]; if(colordark == null){ colordark = "#ffffff"; }
+		backgroundlight = items["backgroundlight"]; if(backgroundlight == null){ backgroundlight = "#ffffff"; }
+		backgrounddark = items["backgrounddark"]; if(backgrounddark == null){ backgrounddark = "#232323"; }
+		backgroundcolor = items["backgroundcolor"]; if(backgroundcolor == null){ backgroundcolor = true; }
+		backgroundimage = items["backgroundimage"]; if(backgroundimage == null){ backgroundimage = false; }
+		backgroundsource = items["backgroundsource"]; if(backgroundsource == null){ backgroundsource = 0; }
+		backgroundsize = items["backgroundsize"]; if(backgroundsize == null){ backgroundsize = 150; }
+		printicon = items["print"]; if(printicon == null){ printicon = false; }
+		password = items["password"]; if(printicon == null){ printicon = false; }
+		enterpassword = items["enterpassword"]; if(enterpassword == null){ enterpassword = ""; }
+
+		if(password == true){
+			var darklayer = document.createElement("div");
+			darklayer.id = "lockscreen";
+			darklayer.addEventListener("contextmenu", (event) => event.preventDefault());
+			document.body.appendChild(darklayer);
+
+			var newinput = document.createElement("input");
+			newinput.type = "password";
+			newinput.id = "inputpass";
+			newinput.setAttribute("autofocus", "autofocus");
+			newinput.placeholder = i18npasswordplaceholder;
+			newinput.addEventListener("mousemove", function(){
+				focuspassword();
+			});
+			newinput.addEventListener("input", function(){
+				checkpassword(this.value);
+			});
+			newinput.addEventListener("keypress", function(e){
+				if(e.key === "Enter"){
+					checkpassword(this.value);
+					if(this.value != enterpassword){
+						newinput.classList.add("animation");
+						window.setTimeout(() => {
+							if(newinput.classList.contains("animation")){
+								newinput.classList.remove("animation");
+							}
+						}, 1000);
+					}
+				}
+			});
+			darklayer.appendChild(newinput);
+			focuspassword();
+		}
+
+		// show remember page
+		var firstmonth = false;
+		var currentDate = new Date().getTime();
+		if(items["firstDate"]){
+			var datestart = items["firstDate"];
+			var dateend = datestart + (30 * 24 * 60 * 60 * 1000);
+			if(currentDate >= dateend){ firstmonth = false; }else{ firstmonth = true; }
+		}else{
+			chrome.storage.sync.set({"firstDate": currentDate});
+			firstmonth = true;
+		}
+
+		if(firstmonth){
+			// show nothing
+			document.getElementById("stefanvdpromo").className = "hidden";
+		}else{
+			if(items["optionskipremember"] != true){
+				document.getElementById("stefanvdpromo").className = ""; // show now always the banner
+			}else{
+				document.getElementById("stefanvdpromo").className = "hidden";
+			}
+		}
+
+		if(!theValue){
+			theValue = "";
+		}
+		maintext.value = theValue;
+
+		if(counter == true){
+			countcharacters();
+			document.getElementById("boxcharacter").className = "";
+		}else{
+			document.getElementById("boxcharacter").className = "hidden";
+		}
+
+		if(copy == true){
+			document.getElementById("copytext").className = "btn-copy";
+		}else{
+			document.getElementById("copytext").className = "hidden";
+		}
+
+		if(speech == true){
+			document.getElementById("texttospeech").className = "";
+		}else{
+			document.getElementById("texttospeech").className = "hidden";
+		}
+
+		if(voices == true){
+			document.getElementById("field").className = "";
+		}else{
+			document.getElementById("field").className = "hidden";
+		}
+
+		if(printicon == true){
+			document.getElementById("printtext").className = "btn-print";
+		}else{
+			document.getElementById("printtext").className = "hidden";
+		}
+
+		if(backgroundimage == true){
+			addbackgroundpaper();
+		}
+
+		// Add stylesheet
+		addstylecode();
+	});
+
+	maintext.onchange = function(){
+		save();
+	};
+	maintext.oninput = function(){
+		countcharacters();
+	};
+
+	document.querySelector(".close").addEventListener("click", () => {
+		document.getElementById("stefanvdpromo").className = "hidden";
+	});
+}
+
+function checkpassword(value){
+	if(value == enterpassword){
+		var elem = document.getElementById("lockscreen");
+		if(elem){
+			elem.parentNode.removeChild(elem);
+		}
+	}
+}
+
+function addstylecode(){
+	const style = document.createElement("style");
+	style.id = "notestyle";
+	style.textContent = `
+	/* Light mode */
+	:root {
+		--ext-primary-background:` + backgroundlight + `;
+		--ext-primary-font-color:#000;
+		--ext-nav-color:` + colorlight + `;
+		--ext-font-size:` + fontsize + `px;
+		--ext-line-height:` + lineheight + `px;
+		--ext-lock-boxshadow: 0 1px 6px 0 rgba(32, 33, 36, .28);
+		--ext-lock-hover:#fbfbfb;
+	}
+	
+	/* Dark mode */
+	@media (prefers-color-scheme: dark) {
+		:root{
+		--ext-primary-background:` + backgrounddark + `;
+		--ext-primary-font-color:#fff;
+		--ext-nav-color:` + colordark + `;
+		--ext-font-size:` + fontsize + `px;
+		--ext-line-height:` + lineheight + `px;
+		--ext-lock-boxshadow: 0 1px 6px 0 rgba(255, 255, 255, 0.28);
+		--ext-lock-hover:#282828;
+		}
+	}
+	`;
+	document.head.appendChild(style);
+}
+
+function removestylecode(){
+	var elem = document.getElementById("notestyle");
+	if(elem){
+		elem.parentNode.removeChild(elem);
+	}
+}
+
+function addbackgroundpaper(){
+	const bck = document.createElement("div");
+	bck.id = "bcknotepaper";
+	bck.className = "notepaper";
+	bck.style.backgroundSize = backgroundsize + "px " + backgroundsize + "px";
+	document.body.appendChild(bck);
+	updatebackgroundpaper();
+}
+
+function removebackgroundpaper(){
+	var elem = document.getElementById("bcknotepaper");
+	if(elem){
+		elem.parentNode.removeChild(elem);
+	}
+}
+
+function updatebackgroundpaper(){
+	var bcknotepaper = document.getElementById("bcknotepaper");
+	if(bcknotepaper){
+		if(backgroundsource == 0){
+			bcknotepaper.style.backgroundImage = "url(" + chrome.runtime.getURL("images/blue-grid.png") + ")";
+		}else if(backgroundsource == 1){
+			bcknotepaper.style.backgroundImage = "url(" + chrome.runtime.getURL("images/black-grid.png") + ")";
+		}else if(backgroundsource == 2){
+			bcknotepaper.style.backgroundImage = "url(" + chrome.runtime.getURL("images/blue-lines.png") + ")";
+		}else if(backgroundsource == 3){
+			bcknotepaper.style.backgroundImage = "url(" + chrome.runtime.getURL("images/black-lines.png") + ")";
+		}
+	}
+}
+
+function countcharacters(){
+	document.getElementById("counter").textContent = document.querySelector("#maintext").value.length;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	const input = document.getElementById("maintext");
+	const main = document.getElementsByTagName("main")[0];
+	const voiceSelect = document.getElementById("voices");
+	let voices;
+	let currentVoice;
+
+	const populateVoices = () => {
+		const availableVoices = speechSynthesis.getVoices();
+		voiceSelect.innerHTML = "";
+
+		availableVoices.forEach((voice) => {
+			const option = document.createElement("option");
+			let optionText = `${voice.name} (${voice.lang})`;
+			if(voice.default){
+				optionText += " [" + i18ndefault + "]";
+				if(typeof currentVoice === "undefined"){
+					currentVoice = voice;
+					option.selected = true;
+				}
+			}
+			if(currentVoice === voice){
+				option.selected = true;
+			}
+			option.textContent = optionText;
+			voiceSelect.appendChild(option);
+		});
+		voices = availableVoices;
+	};
+
+	populateVoices();
+	if(speechSynthesis.onvoiceschanged !== undefined){
+		speechSynthesis.onvoiceschanged = populateVoices;
+	}
+
+	voiceSelect.addEventListener("change", (event) => {
+		const selectedIndex = event.target.selectedIndex;
+		currentVoice = voices[selectedIndex];
+	});
+
+	var utterance;
+	document.getElementById("startspeech").addEventListener("click", (event) => {
+		event.preventDefault();
+		const toSay = input.value.trim();
+		utterance = new SpeechSynthesisUtterance(toSay);
+		utterance.voice = currentVoice;
+		utterance.rate = 0.85;
+		utterance.addEventListener("start", () => {
+			main.classList.add("speaking");
+			document.getElementById("startspeech").classList.add("hidden");
+			document.getElementById("stopspeech").classList.remove("hidden");
+		});
+		utterance.addEventListener("end", () => {
+			main.classList.remove("speaking");
+			document.getElementById("startspeech").classList.remove("hidden");
+			document.getElementById("stopspeech").classList.add("hidden");
+		});
+		speechSynthesis.speak(utterance);
+	});
+
+	document.getElementById("stopspeech").addEventListener("click", (event) => {
+		event.preventDefault();
+		speechSynthesis.cancel();
+		document.getElementById("startspeech").classList.remove("hidden");
+		document.getElementById("stopspeech").classList.add("hidden");
+	});
+
+	document.getElementById("copytext").addEventListener("click", () => {
+		const input = document.getElementById("maintext");
+		input.select();
+		document.execCommand("copy");
+		if(showingcopybadge == false){
+			showcopytextbadge();
+		}
+	});
+
+	document.getElementById("printtext").addEventListener("click", () => {
+		print();
+	});
+
+});
+
+var showingcopybadge = false;
+function showcopytextbadge(){
+	var div = document.createElement("div");
+	div.setAttribute("id", "stefanvdremoteadd");
+	div.className = "stefanvdremote";
+	document.body.appendChild(div);
+
+	var h3 = document.createElement("h3");
+	h3.innerText = i18ntitelcopytext;
+	div.appendChild(h3);
+
+	var p = document.createElement("p");
+	p.innerText = i18ndescopytext;
+	div.appendChild(p);
+	showingcopybadge = true;
+
+	window.setTimeout(function(){
+		var element = document.getElementById("stefanvdremoteadd");
+		element.parentNode.removeChild(element);
+		showingcopybadge = false;
+	}, 4000);
+}
+
+document.addEventListener("DOMContentLoaded", init, false);
+
+chrome.runtime.onMessage.addListener(function(request){
+	if(request.msg == "setnotetext"){
+		document.querySelector("#maintext").value = request.value;
+	}else if(request.msg == "setcounter"){
+		if(request.value == true){
+			countcharacters();
+			document.getElementById("boxcharacter").className = "";
+		}else{
+			document.getElementById("boxcharacter").className = "hidden";
+		}
+	}else if(request.msg == "setcopy"){
+		if(request.value == true){
+			document.getElementById("copytext").className = "btn-copy";
+		}else{
+			document.getElementById("copytext").className = "hidden";
+		}
+	}else if(request.msg == "setspeech"){
+		if(request.value == true){
+			document.getElementById("texttospeech").className = "";
+		}else{
+			document.getElementById("texttospeech").className = "hidden";
+		}
+	}else if(request.msg == "setvoices"){
+		if(request.value == true){
+			document.getElementById("field").className = "";
+		}else{
+			document.getElementById("field").className = "hidden";
+		}
+	}else if(request.msg == "setfontsize"){
+		fontsize = request.value;
+		removestylecode();
+		addstylecode();
+	}else if(request.msg == "setcolorlight"){
+		colorlight = request.value;
+		removestylecode();
+		addstylecode();
+	}else if(request.msg == "setcolordark"){
+		colordark = request.value;
+		removestylecode();
+		addstylecode();
+	}else if(request.msg == "setbackgroundlight"){
+		backgroundlight = request.value;
+		removestylecode();
+		addstylecode();
+	}else if(request.msg == "setbackgrounddark"){
+		backgrounddark = request.value;
+		removestylecode();
+		addstylecode();
+	}else if(request.msg == "setbackgroundcolor"){
+		if(request.value == true){
+			removebackgroundpaper();
+		}else{
+			addbackgroundpaper();
+		}
+	}else if(request.msg == "setbackgroundsource"){
+		backgroundsource = request.value;
+		updatebackgroundpaper();
+	}else if(request.msg == "setbackgroundsize"){
+		var bcknotepaper = document.getElementById("bcknotepaper");
+		if(bcknotepaper){
+			bcknotepaper.style.backgroundSize = request.value + "px";
+		}
+	}else if(request.msg == "setprint"){
+		if(request.value == true){
+			document.getElementById("printtext").className = "btn-print";
+		}else{
+			document.getElementById("printtext").className = "hidden";
+		}
+	}
+});
