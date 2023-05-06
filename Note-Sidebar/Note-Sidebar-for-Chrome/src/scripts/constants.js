0ccee68077f677f0e5234fa97d76dd1215f5f651
Stefan Van Damme <20127018+stefanvd@users.noreply.github.com>
2023-05-06 11:58:34 +0200

    Update extension

--- Note-Sidebar/Note-Sidebar-for-Chrome/src/scripts/constants.js
+++ Note-Sidebar/Note-Sidebar-for-Chrome/src/scripts/constants.js
@@ -1,20 +1,20 @@
 /* eslint-disable no-unused-vars */
-const exbrowser = "opera";
+const exbrowser = "chrome";
 // values: "chrome", "safari", "edge", "firefox", "opera", "yandex", "whale"
 const linkredirectionoptions = "https://www.stefanvd.net/project/note-sidebar/browser/options/";
 const linkdeveloperwebsite = "https://www.stefanvd.net";
-const linkproduct = "https://store.whale.naver.com/detail/" + chrome.runtime.id;
+const linkproduct = "https://chrome.google.com/webstore/detail/" + chrome.runtime.id;
 const linkdonate = "https://www.stefanvd.net/donate/";
-const writereview = "https://store.whale.naver.com/detail/" + chrome.runtime.id;
-const linkchangelog = "https://www.stefanvd.net/project/note-sidebar/browser/whale/changelog/";
+const writereview = "https://chrome.google.com/webstore/detail/" + chrome.runtime.id + "/reviews";
+const linkchangelog = "https://www.stefanvd.net/project/note-sidebar/browser/google-chrome/changelog/";
 const linktranslate = "https://www.stefanvd.net/project/translate/";
 const linksupport = "https://www.stefanvd.net/support/";
-const linkwelcome = "https://www.stefanvd.net/project/note-sidebar/browser/whale/welcome/";
-const linkuninstall = "https://www.stefanvd.net/project/note-sidebar/browser/whale/uninstall/";
-const linkguide = "https://www.stefanvd.net/project/note-sidebar/browser/whale/guide/";
-const linkproductdescription = "https://www.stefanvd.net/project/note-sidebar/browser/";
-const browsernewtab = "whale://newtab/";
-const browserstore = "https://store.whale.naver.com";
+const linkwelcome = "https://www.stefanvd.net/project/note-sidebar/browser/google-chrome/welcome/";
+const linkuninstall = "https://www.stefanvd.net/project/note-sidebar/browser/google-chrome/uninstall/";
+const linkguide = "https://www.stefanvd.net/project/note-sidebar/browser/google-chrome/guide/";
+const linkproductdescription = "https://www.stefanvd.net/project/note-sidebar/browser-extension/";
+const browsernewtab = "chrome://newtab/";
+const browserstore = "https://chrome.google.com";
 const linkyoutube = "https://www.youtube.com/@stefanvandamme?sub_confirmation=1";
 const devmode = true;
 const devdonate = false;
