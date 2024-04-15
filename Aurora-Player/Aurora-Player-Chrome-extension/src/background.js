7e17fdf80cf6537f6c040b4b922726835fa1fd2a
Stefan Van Damme <20127018+stefanvd@users.noreply.github.com>
2024-04-15 23:27:20 +0200

    Update

---
+++ Aurora-Player/Aurora-Player-Chrome-extension/src/background.js
@@ -0,0 +1,19 @@
+//================================================
+/*
+
+Aurora Player
+Open the video to the video player app
+Copyright (C) 2024 Stefan vd
+www.stefanvd.net
+
+*/
+//================================================
+
+chrome.action.onClicked.addListener((tab) => {
+	if(tab.url.startsWith("http://") || tab.url.startsWith("https://")){
+		chrome.scripting.executeScript({
+			target: {tabId: tab.id},
+			files: ["currentvideo.js"]
+		});
+	}
+});
