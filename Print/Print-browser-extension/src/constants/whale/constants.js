cd6394945679fb7f98c791f029ca8bf51e456a34
Stefan Van Damme <20127018+stefanvd@users.noreply.github.com>
2024-07-20 23:24:12 +0200

    Update

---
+++ Print/Print-browser-extension/src/constants/whale/constants.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+const exbrowser = "whale";
+// values: "chrome", "safari", "edge", "firefox", "opera", "yandex", "whale"
+const linkredirectionoptions = "https://www.stefanvd.net/project/print/browser/options/";
+const linkdeveloperwebsite = "https://www.stefanvd.net";
+const linkproduct = "https://store.whale.naver.com/detail/" + chrome.runtime.id;
+const linkdonate = "https://www.stefanvd.net/donate/";
+const writereview = "https://store.whale.naver.com/detail/" + chrome.runtime.id;
+const linkchangelog = "https://www.stefanvd.net/project/print/browser/whale/changelog/";
+const linktranslate = "https://www.stefanvd.net/project/translate/";
+const linksupport = "https://www.stefanvd.net/support/";
+const linkwelcome = "https://www.stefanvd.net/project/print/browser/whale/welcome/";
+const linkuninstall = "https://www.stefanvd.net/project/print/browser/whale/uninstall/";
+const linkguide = "https://www.stefanvd.net/project/print/browser/whale/guide/";
+const linkproductdescription = "https://www.stefanvd.net/project/print/browser/";
+const browsernewtab = "whale://newtab/";
+const browserstore = "https://store.whale.naver.com";
+const linkyoutube = "https://www.youtube.com/@stefanvandamme?sub_confirmation=1";
+const devmode = true;
+const devdonate = false;
